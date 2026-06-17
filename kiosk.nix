05cf1723a3029b8c764e15d8630752279ae86ba4
Louis Raymond <louis.raymond2017@gmail.com>
2026-06-16 20:10:56 -0400

    ssh key

--- kiosk.nix
+++ kiosk.nix
@@ -43,15 +43,29 @@
     enable = true;
     user = "kiosk";
     program = "${pkgs.writeShellScript "firefox-kiosk" ''
-      exec ${pkgs.firefox}/bin/firefox \
+      ${pkgs.firefox}/bin/firefox \
         --kiosk \
-        https://ctfd.summercamp.dciets.com/scoreboard
+        https://ctfd.summercamp.dciets.com/scoreboard &
+      FIREFOX_PID=$!
+
+      while kill -0 "$FIREFOX_PID" 2>/dev/null; do
+        sleep 10
+        ${pkgs.wtype}/bin/wtype -k F5
+      done
+
+      wait "$FIREFOX_PID"
     ''}";
+  };
+
+  systemd.services."cage-tty1".serviceConfig = {
+    Restart = "always";
+    RestartSec = "3s";
   };
 
   environment.systemPackages = with pkgs; [
     firefox
     cage
+    wtype
   ];
 
   services.logind.settings.Login = {
@@ -59,6 +73,15 @@
     HandleLidSwitch = "ignore";
     HandleLidSwitchExternalPower = "ignore";
   };
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+  };
+
+  users.users.kiosk.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ+RLnMHSHLIy8iHgBY0Xkiv3u1zpXzhuXLOwWSvswuR louis@nixos"
+  ];
 
   time.timeZone = "America/Toronto";
   i18n.defaultLocale = "fr_CA.UTF-8";
